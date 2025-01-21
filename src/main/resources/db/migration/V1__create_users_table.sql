2b22e168d9912407c1ed2b14c255b99b9894325e
Matheus Macedo <matheus.macedo.developer@gmail.com>
2025-01-21 00:22:44 -0300

    feat: add migration to create users table

---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    user_id UUID PRIMARY KEY,
+    user_name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    password_hash BYTEA NOT NULL,
+    password_salt BYTEA NOT NULL,
+    weight NUMERIC(6,3) NOT NULL,
+    basal_metabolic_rate SMALLINT NOT NULL,
+    activity_level_coefficient SMALLINT NOT NULL
+);
