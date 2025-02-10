d5720652bbd804f7a141d6db085406e509e111a7
Matheus Macedo <matheus.macedo.developer@gmail.com>
2025-02-09 23:36:36 -0300

    feat: add input-with-label component

--- frontend/src/app/features/outside/components/input-with-label/input-with-label.component.ts
+++ frontend/src/app/features/outside/components/input-with-label/input-with-label.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
-  selector: 'app-input-with-label',
+  selector: 'input-with-label',
   imports: [],
   templateUrl: './input-with-label.component.html',
   styleUrl: './input-with-label.component.scss'
 })
 export class InputWithLabelComponent {
-
+  label = input.required<String>();
+  placeholder = input.required<String>();
 }
